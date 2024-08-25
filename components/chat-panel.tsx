6615ccc6bbd6f19ee6c02b5122b13e54e18a6d9f
orzumurod parmonov <orzumurodparmonov@gmail.com>
2024-08-25 21:10:37 +0500

    .env created but ignored 2

--- components/chat-panel.tsx
+++ components/chat-panel.tsx
@@ -114,7 +114,7 @@ export function ChatPanel({ messages, query }: ChatPanelProps) {
   return (
     <div
       className={
-        'bottom-0 left-0 right-0 top-0 mx-auto min-h-[50vh] flex flex-col items-center justify-center'
+        'bottom-0 left-0 right-0 top-0 mx-auto min-h-[50vh] min-w-full flex flex-col items-center justify-center'
       }
     >
       <form onSubmit={handleSubmit} className="max-w-2xl w-full px-6">
